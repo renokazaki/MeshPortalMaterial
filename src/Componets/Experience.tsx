e2023faafdc4addd2a80f66459c417b622690d34
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-12-07 16:00:31 +0900

    create MeshPortalMaterial by TypeScript

--- src/Componets/Experience.tsx
+++ src/Componets/Experience.tsx
@@ -3,7 +3,9 @@ import {
   useTexture,
   MeshPortalMaterial,
   RoundedBox,
+  Float,
   CameraControls,
+  Text,
   PortalMaterialType,
 } from "@react-three/drei";
 import { useEffect, useRef, useState } from "react";
@@ -12,16 +14,19 @@ import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 
 const Experience = () => {
-  const model = useGLTF("/public/model/1.glb");
-  const Texture = useTexture("/public/texture/1.png");
+  const model = useGLTF("./public/model/1.glb");
+  const Texture = useTexture("./public/texture/1.png");
 
   const [active, setActive] = useState(false);
-  // 修正された useRef
-  const meshPortalMaterialRef = useRef<PortalMaterialType | null>(null);
+
+  // 修正済み useRef
   const cameraControlsRef = useRef<CameraControls | null>(null);
+  const meshPortalMaterialRef = useRef<PortalMaterialType | null>(null);
+
   const handleClick = () => {
     setActive(!active);
   };
+
   useFrame((_, delta) => {
     if (meshPortalMaterialRef.current) {
       easing.damp(
@@ -37,9 +42,9 @@ const Experience = () => {
   useEffect(() => {
     if (cameraControlsRef.current) {
       if (active) {
-        cameraControlsRef.current.setLookAt(0, 0, 10, 0, 0, 0, true);
+        cameraControlsRef.current.setLookAt(0, 0, 3, 0, 0, 0, true);
       } else {
-        cameraControlsRef.current.setLookAt(0, 0, -5, 0, 0, 0, true);
+        cameraControlsRef.current.setLookAt(0, 0, 5, 0, 0, 0, true);
       }
     }
   }, [active]);
@@ -48,15 +53,22 @@ const Experience = () => {
     <>
       <CameraControls ref={cameraControlsRef} />
 
-      <RoundedBox args={[2, 3, 0.1]} radius={0.2} onDoubleClick={handleClick}>
-        <MeshPortalMaterial ref={meshPortalMaterialRef}>
-          <primitive object={model.scene} scale={0.4} position-y={0.4} />
-          <mesh>
-            <sphereGeometry args={[20, 63, 63]} />
-            <meshBasicMaterial map={Texture} side={THREE.BackSide} />
-          </mesh>
-        </MeshPortalMaterial>
-      </RoundedBox>
+      <Text position={[0, 1.5, 0.1]} fontSize={0.6}>
+        Eggs
+        <meshBasicMaterial toneMapped={false} />
+      </Text>
+
+      <Float>
+        <RoundedBox args={[3, 4, 0.1]} radius={0.2} onDoubleClick={handleClick}>
+          <MeshPortalMaterial blend={active ? 1 : 0}>
+            <primitive object={model.scene} scale={0.4} position-y={0.4} />
+            <mesh>
+              <sphereGeometry args={[3, 500, 500]} />
+              <meshBasicMaterial map={Texture} side={THREE.BackSide} />
+            </mesh>
+          </MeshPortalMaterial>
+        </RoundedBox>
+      </Float>
     </>
   );
 };
